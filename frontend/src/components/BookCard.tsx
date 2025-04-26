ce1e89799c26afa073be1891674735eb908245d8
Aurélie <aurelie.demure.ad@gmail.com>
2025-04-26 15:50:01 +0200

    fix/style

--- frontend/src/components/BookCard.tsx
+++ frontend/src/components/BookCard.tsx
@@ -9,31 +9,32 @@ const BookCard = ({ books }: BookCardProps) => {
   return (
     <div className="flex flex-col items-center mb-4 gap-4">
       {books?.map((book) => (
-        <Card className="w-[350px] flex " key={book.id}>
-          <CardHeader className="flex-col items-center gap-4">
-            <div className="flex items-center">
+        <Card className="w-[350px] flex !py-0 !pb-0" key={book.id}>
+          <CardHeader className="flex-col items-center !pb-2 !m-0 !pl-2">
+            <div className="flex items-center space-x-2">
               {book.image ? (
                 <img
                   src={book.image}
                   alt={book.title}
-                  className="w-auto h-22 m-2"
+                  className="w-auto h-24 m-2 mt-3 rounded-md"
                 />
               ) : (
-                <BookText className="text-muted-foreground w-auto h-20" />
+                <BookText className="text-muted-foreground w-auto h-20 mt-2" />
               )}
               <div>
                 <h2 className="text-lg font-semibold">{book.title}</h2>
                 <p>{book.author}</p>
               </div>
-              <div className="flex-1" />
-              <p className="text-sm text-muted-foreground">
-                {book.borrowedBy && `Emprunté par ${book.borrowedBy}`}
-                {book.borrowedAt &&
-                  `le ${book.borrowedAt.toLocaleDateString()}`}
-              </p>
+
+              {book.borrowedBy && (
+                <p className="text-sm text-muted-foreground">
+                  `Emprunté par ${book.borrowedBy}` `le $
+                  {book.borrowedAt.toLocaleDateString()}`
+                </p>
+              )}
             </div>
             <div className="flex items-center justify-end gap-2">
-              {book.isRead && <BookOpenCheck />}
+              {!book.isRead && <BookOpenCheck />}
               {book.toRead && <BookOpen />}
               {book.isFavorite && <Heart className="text-red-500" />}
             </div>
